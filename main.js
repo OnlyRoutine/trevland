6a3885647883c299ac8952b54579a78ac06eeb95
Vlad Popov <scientific1atheism@gmail.com>
2021-08-27 19:57:31 +0300

    make site more responsive

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const scrollLinksContainer = document.querySelector("header div section div nav ul")
-    console.log(scrollLinksContainer)
+// const scrollLinksContainer = document.querySelector("header div section div nav ul")
+//     console.log(scrollLinksContainer)
 
-scrollLinksContainer.children.forEach((item) => {
-    item.children[0].addEventListener("click", (event) => {
-        event.preventDefault()
-        window.scrollTo({ top: document.getElementById(item.children[0].href.split("#")[1]).offsetTop, behavior: 'smooth'});
-    })
-});
+// scrollLinksContainer.children.forEach((item) => {
+//     item.children[0].addEventListener("click", (event) => {
+//         event.preventDefault()
+//         window.scrollTo({ top: document.getElementById(item.children[0].href.split("#")[1]).offsetTop, behavior: 'smooth'});
+//     })
+// });
